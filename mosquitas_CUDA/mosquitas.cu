70e47c1f414b7c6635192a713370d2d52ef974a9
GramajoAna <63738296+GramajoAna@users.noreply.github.com>
2020-10-27 21:12:19 -0300

    descacharrado incluido-sin transferencia de tacho

--- mosquitas_CUDA/mosquitas.cu
+++ mosquitas_CUDA/mosquitas.cu
@@ -1,5 +1,5 @@
-#include <Random123/philox.h> // philox headers
-#include <Random123/u01.h>    // to get uniform deviates [0,1]
+#include <philox.h> // philox headers
+#include <u01.h>    // to get uniform deviates [0,1]
 typedef r123::Philox2x32 RNG; // particular counter-based RNG
 
 #include <cstdlib>
@@ -14,15 +14,14 @@ typedef r123::Philox2x32 RNG; // particular counter-based RNG
 
 int tiempo_entre_oviposiciones(int dia){
 	int t;
-	//defino tiempos de oviposiciรณn y maduraciรณn en funciรณn de la temperatura	
-	if(dia >= 1 && dia < 80) t=TOVIP3;      //<T>=18 // acรก es necesario definirlo tomando el extremo
+	//defino tiempos de oviposición y maduración en función de la temperatura	
+	if(dia >= 1 && dia < 80) t=TOVIP3;      //<T>=18 // acá es necesario definirlo tomando el extremo
 	if(dia >= 80 && dia <= 140)t=TOVIP2b;   //<T>=23
   	if(dia > 140 || dia < 260) t=TOVIP1;    //<T>=30
  	if(dia >= 260 && dia <= 320)t=TOVIP2a;  //<T>=27
  	if(dia > 320) t=TOVIP3;                 //<T>=18
 
 	return t;}
-
 
 __global__ void kernel_reproducir(int *estado, int *edad, int *tacho,int *TdV, int *pupacion,int *manzana, int *N_mobil, int dia, int tovip, int *nacidos)
 {                                   
@@ -82,7 +81,6 @@ __global__ void matar_kernel(int *estado, int *edad, int *tacho,int *pupacion, i
 		    if (edad[id] == pupacion[id]){if(azar < MORPUPAD)estado[id]=ESTADOMUERTO;}
 		    if (edad[id] > pupacion[id]){if(azar < MORAD)estado[id]=ESTADOMUERTO;}
 		    if (edad[id] >= TdV[id]){estado[id]=ESTADOMUERTO;}
-//TdV[id]
 	}
 };
 
@@ -91,11 +89,11 @@ __global__ void descacharrado_kernel(int *estado, int *edad, int *tacho, int *pu
     	int N=N_mobil[0];
 	    int id = blockIdx.x*blockDim.x + threadIdx.x;
 
-	    if(id<N){// esta condicion es igual a decir que la mosquita está viva
+	    if(id<N){// esta condicion es igual a decir que la mosquita estรก viva
 	    if (edad[id] < pupacion[id] && tacho[id] == ntach)estado[id]=ESTADOMUERTO;
     	}
 };
-
+//elimine el estado[id]=ESTADOVIVO, ya que al final sólo quedan las mosquitas vivas
 __global__ void envejecer_kernel(int *estado, int *edad,int *pupacion,int *N_mobil,int dia)
 {
     	int N=N_mobil[0];
@@ -103,13 +101,11 @@ __global__ void envejecer_kernel(int *estado, int *edad,int *pupacion,int *N_mob
 
         if(id<N){
             if(dia < 80 || dia > 320){
-                if(estado[id] == ESTADOVIVO && edad[id] > pupacion[id])edad[id]++;} //ADULTAS
+                if(edad[id] > pupacion[id])edad[id]++;} //ADULTAS
   		    else{
-  		        if (estado[id]== ESTADOVIVO)edad[id]++;}
+  		        edad[id]++;}
     	}
 };
-
-// NUEVO: otro functorcito usado para las estadisticas desagregadas
 
 // struct para generar randoms uniformes
 struct uniformRanInt{
@@ -151,6 +147,7 @@ struct acuaticoeneltacho{
 	}
 };
 
+//MODIFIQUÉ EL ORDEN DE LAS TUPLA
 struct acuaticoeneltachoANA{
 	int m;
 
@@ -158,18 +155,15 @@ struct acuaticoeneltachoANA{
     
 	__device__ bool operator()(thrust::tuple<int,int,int> tupla)
 	{
-        int pupacion=thrust::get<0>(tupla);
+        int tacho=thrust::get<0>(tupla);
         int edad=thrust::get<1>(tupla);
-        int tacho=thrust::get<2>(tupla);
-		return (edad < pupacion && tacho==m);
+        int pupacion=thrust::get<2>(tupla);
+		return (tacho==m && edad < pupacion);
 	}
 };
 
-
-
-// functorcito para contar adultos en la población
-
-struct poblacion_adultos{
+//functorcito para contar adultos en la población
+struct poblacion_1{
 	__device__ bool operator()(thrust::tuple<int,int> tupla)
 	{
         int edad=thrust::get<0>(tupla);//NUEVO: cambie el orden de la tupla
@@ -179,7 +173,7 @@ struct poblacion_adultos{
 };
 
 // functorcito para contar acuáticos en la población
-struct poblacion_acuaticos{
+struct poblacion_2{
 	__device__ bool operator()(thrust::tuple<int,int> tupla)
 	{
         int edad=thrust::get<0>(tupla); //NUEVO:cambie el orden de la tupla
@@ -188,14 +182,13 @@ struct poblacion_acuaticos{
 	}
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////
-// Clase bichos: toda la info sobre todos los bichos, y sus funciones
 struct bichos{
 
-	thrust::device_vector<int> estado; // Vivo o Muerto 0/1 
-	thrust::device_vector<int> edad; // tiene num de mosqu elementos y los valores van de 0 a MAXIMAEDAD   
-	thrust::device_vector<int> tacho; // numero de tacho en que se encuentra cada mosquita valores=0 a NUMEROTACHOS   
-	thrust::device_vector<int> TdV;  //tiempo de vida de cada mosquita
+
+	thrust::device_vector<int> estado;  // Vivo o Muerto 0/1 
+	thrust::device_vector<int> edad;    //edad que tiene la mosquita   
+	thrust::device_vector<int> tacho;  // numero de tacho en que se encuentra cada mosquita valores=0 a NUMEROTACHOS   
+	thrust::device_vector<int> TdV;     //tiempo de vida de cada mosquita
 	thrust::device_vector<int> pupacion; //dia de paso de pupa a adulta de cada mosquita
 	thrust::device_vector<int> manzana; //numero de manzana de cada mosquita
 	thrust::device_vector<int> nacidos; // tiene el num de tachos elementos, numero de nacidos por tacho
@@ -246,21 +239,16 @@ struct bichos{
 	raw_pupacion=thrust::raw_pointer_cast(pupacion.data());
 	raw_nacidos=thrust::raw_pointer_cast(nacidos.data());
 
-    //std::cout<<"VoM\ttacho\tedad\tTdV\ttpupad\tmanzana" << std::endl;
-	/*condiciones iniciales donde N sera el numero de bichos*/
+	/*condiciones iniciales*/
  	for(int i=0;i < N_;i++){
-		estado[i] = ESTADOVIVO; 		    //todos vivos inicialmente
-		tacho[i] = i;				        //tacho en el que se encuentra la mosquita
-		edad[i] = ran2(&semilla)*7+19; 	//edad: son todas adultas al principio 
-		pupacion[i] = TPUPAD-2+(ran2(&semilla)*5);//dia de pupacion (entre los 15 y 19 dias)
-		TdV[i] = ran2(&semilla)*6+27 ;	//tiempo de vida de 27 a 32
-		manzana[i] = (int) (i/5);         //manzana en la que se encuentra
-		
-	std::cout << estado[i] << "\t" << tacho[i] << "\t" << edad[i] << "\t" << TdV[i] << "\t" << pupacion[i] << "\t" << manzana[i] << "\n";
-	std::cout << "pupacion" << pupacion[i]<< std::endl;
-}
-
-std::cout << "inicializacion lista" << std::endl;
+		estado[i]=ESTADOVIVO; 		        //todos vivos inicialmente
+		tacho[i]=i;			        	    //tacho en el que se encuentra la mosquita
+		edad[i]=ran2(&semilla)*7+19; 		//edad 
+		pupacion[i]=TPUPAD - 2 + (ran2(&semilla)*5);    //dia de pupacion (entre los 15 y 19 dias)
+		TdV[i]=ran2(&semilla)*6+27 ;	 ón//tiempo de vida de 27 a 32
+		manzana[i]=(int) (i/5);
+		std::cout << estado[i] << "\t" << tacho[i] << "\t" << edad[i] << "\t" << TdV[i] << "\t" << pupacion[i] << "\n";
+	}
 
 	N_mobil[0]=N_;
 	};	
@@ -269,21 +257,35 @@ std::cout << "inicializacion lista" << std::endl;
 	void mortalidades(int dia){
 
 	int N=N_mobil[0];
-	
+	//mortalidades varias y muerte por vejez
 	    matar_kernel<<<(N+256-1)/256,256>>>(raw_estado,raw_edad,raw_tacho,raw_pupacion,raw_TdV,raw_N_mobil, dia);	
 		cudaDeviceSynchronize();
 	};
-	
+
 	//descachrarrado
 	void descacharrado(int dia,int descach){
-	int N=N_mobil[0];	
+	int N=N_mobil[0];
+	    // SERIAL
+		/*	if(dia%7 == 0 && dia > 120 && dia < 320){
+  			for(int itach=0;itach < descach;itach++){
+    		    int ntach=ran2(&semilla)*NUMEROTACHOS;
+				for(int i=0;i < N;i++){
+  					if (estado[i] == ESTADOVIVO && edad[i] < pupacion[i] && tacho[i] == ntach)estado[i]=ESTADOMUERTO;
+				}
+			}    	
+   		}*/
+   		//CUDA
 		if(dia%7 == 0 && dia > 120 && dia < 320){
   			for(int itach=0;itach < descach;itach++){
-    			int ntach=ran2(&semilla)*NUMEROTACHOS;
+  			    /*ESTO ES RARO: al parecer no le gusta ntach=ran2(&semilla)*NUMEROTACHOS el cual sortea nros random entre [0,NUMEROTACHOS)(1), asi que lo que hice fue tirar nros random entre [0,NUMEROTACHOS] y si ntach=NUMEROTACHOS=5 entonces vuelvo a sortear otro nro random que caiga  en el intervalo (1), haciendo esto la poblacion de mosquitas no me cae a 0, probe con varias semillaglobal y el resultado no es tan sensible a la variación de la semilla, algo que si observabamos antes. Ahora, esto está bien ?*/
+    			int ntach=ran2(&semilla)*NUMEROTACHOS + 1;//ntach=(0,1,2,3,4) + 1 
+    			if(ntach==NUMEROTACHOS)ntach=ran2(&semilla)*NUMEROTACHOS;// ntach= (0,1,2,3,4)
   				descacharrado_kernel<<<(N+256-1)/256,256>>>(raw_estado, raw_edad, raw_tacho, raw_pupacion, raw_N_mobil,dia,ntach);
+  				cudaDeviceSynchronize();
 			}    	
    		}
 	};
+
 
     //nacimientos
 	void reproducir(int dia,int tovip){
@@ -295,7 +297,7 @@ std::cout << "inicializacion lista" << std::endl;
 	}else{
 
 	//nacimientos
-	std::cout << "antes kernel reproducir " << std::endl;
+	//std::cout << "antes kernel reproducir " << std::endl;
 	//reinicializo en cero los nacidos en el paso anterior que ahora ya no son mas nacidos porque crecieron 
 	thrust::fill(nacidos.begin(),nacidos.end(),0);
 
@@ -303,54 +305,49 @@ std::cout << "inicializacion lista" << std::endl;
 	kernel_reproducir<<<(indice+256-1)/256,256>>>(raw_estado,raw_edad,raw_tacho,raw_TdV,raw_pupacion,raw_manzana,raw_N_mobil,dia,tovip,raw_nacidos);
 	cudaDeviceSynchronize();
 
-	std::cout << "despues kernel reproducir " << std::endl; 
+	//std::cout << "despues kernel reproducir " << std::endl; 
 		// agrego todos los nacidos al final del array original, tacho a tacho
 		int index=indice;
 		for(int m=0;m<NUMEROTACHOS;m++){
-			//std::cout << "listo nacidos " << std::endl; 
+		    	//std::cout << "listo nacidos " << std::endl; 
  
             //antiguos=thrust::count_if(tacho.begin(),tacho.begin()+N,iguala(m));
 			//calculo el nunmero de acuaticos en cada tacho
-            int antiguos=thrust::count_if(
+            /*int antiguos=thrust::count_if(
                 thrust::make_zip_iterator(thrust::make_tuple(tacho.begin(),edad.begin())),
                 thrust::make_zip_iterator(thrust::make_tuple(tacho.begin()+indice,edad.begin()+indice)),
                 acuaticoeneltacho(m,TPUPAD)
-			);
-
-			/* //NUEVO: cuando el TPUPAD es variable, indice=nro de bichos hasta el momento
-            int antiguosANA=thrust::count_if(
-                thrust::make_zip_iterator(thrust::make_tuple(edad.begin(), pupacion.begin(),tacho.begin())),
-                thrust::make_zip_iterator(thrust::make_tuple(edad.begin() + indice, pupacion.begin() + indice, tacho.begin() + indice)),
-                acuaticoeneltachoANA(m)
-            ); */
+			);*/
 			
-			std::cout << "Acuaticos en Tacho " << antiguos << " TACHO "<< m <<std::endl;
+			//Comenté el anterior porque el código actualizado que me pasó fabi considera el TPUPAD variable, indice=nro de bichos hasta el momento
+            int antiguos=thrust::count_if(
+                thrust::make_zip_iterator(thrust::make_tuple(tacho.begin(), edad.begin(),pupacion.begin())),
+                thrust::make_zip_iterator(thrust::make_tuple(tacho.begin() + index, edad.begin() + index, pupacion.begin() + index)),
+                acuaticoeneltachoANA(m)
+            ); 
+            
+			//std::cout << "Acuaticos en Tacho " << antiguos << " TACHO "<< m <<std::endl;
           	//los nuevos vienen del kernel reproducir  
             int nuevos=nacidos[m];
-
+            //me parece que si sucede esta condición  la mosquita tiene que irse a otro tachito, se podria hilar la transferencia de tacho aquí
 			if(nuevos+antiguos>SAT){
 				nuevos=SAT-antiguos;	
 			};
 
-			std::cout << "NUEVOS NACIDOS " << nuevos <<" TACHO "<< m <<std::endl; 
+			//std::cout << "NUEVOS NACIDOS " << nuevos <<" TACHO "<< m <<std::endl; 
 
-            thrust::fill(estado.begin()+index,estado.begin()+index+nuevos,ESTADOVIVO);	        //NUEVO	
-			thrust::fill(edad.begin()+index,edad.begin()+index+nuevos,0);		                //NUEVO	
-			thrust::fill(tacho.begin()+index,tacho.begin()+index+nuevos,m); 	                //nacen en el tacho m
-
-			//thrust::fill(pupacion.begin()+index,pupacion.begin()+index+nuevos,15);
+            thrust::fill(estado.begin()+index,estado.begin()+index+nuevos,ESTADOVIVO);	        //estado del nacido	
+			thrust::fill(edad.begin()+index,edad.begin()+index+nuevos,1);		                //edad del nacido: dia 1 y no 0 	
+			thrust::fill(tacho.begin()+index,tacho.begin()+index+nuevos,m); 	                //tacho en el que nace
 
 			// index en counting iteraror necesario para distintos randoms en cada tacho
 			thrust::transform(
-				thrust::make_counting_iterator(index),thrust::make_counting_iterator(index+nuevos),
+				thrust::make_counting_iterator(index),thrust::make_counting_iterator(index+nuevos),//edad en que se vuelve pupa
 				pupacion.begin()+index,uniformRanInt(15,5,dia)
 			);
 		
-			// + rand()%5);   //NUEVO:descomento y se pincha el codigo
-		//	thrust::fill(TdV.begin()+index,TdV.begin()+index+nuevos,30);
-			//rand()%6 + 27);               //NUEVO
 			thrust::transform(
-				thrust::make_counting_iterator(index),thrust::make_counting_iterator(index+nuevos),
+				thrust::make_counting_iterator(index),thrust::make_counting_iterator(index+nuevos),//tiempo de vida
 				TdV.begin()+index,uniformRanInt(27,6,dia)
 			);
 
@@ -371,7 +368,7 @@ std::cout << "inicializacion lista" << std::endl;
 	}	
 	
 	};
-
+	
     //Recalcular -> eliminar muertos y dejar vivos
     void recalcularN(){
 
@@ -380,15 +377,12 @@ std::cout << "inicializacion lista" << std::endl;
 		// ordenamos segun estado 0-vivo, 1-muerto
 		int N=N_mobil[0];
 		thrust::sort_by_key(estado.begin(), estado.begin() + N,zip_iterator);		
-		//std::cout << "N en recalcular N " << N <<std::endl; 
+	
 		// y ahora determinamos la posicion del primer muerto = N_mobil
 		auto iter=thrust::find(estado.begin(),estado.begin() + N, ESTADOMUERTO);
-		
 		N_mobil[0]= iter-estado.begin();//me da la longitud del vector
-		std::cout << "N_mobil " << N_mobil[0] <<std::endl;
 	};
 	
-	// Numero de mosquitas vivas
 	int vivos(int dia){
 
 	int N=N_mobil[0];
@@ -397,15 +391,15 @@ std::cout << "inicializacion lista" << std::endl;
 	return poblacion;
 	};
 
-    //población de acuáticos
+    //poblaciรณn de acuรกticos
 	int acuaticos(int dia){
 
 	int N=N_mobil[0];
-	
+	//el predicado poblacion_2()corresponde a los acuaticos
     int ac=thrust::count_if(
                 thrust::make_zip_iterator(thrust::make_tuple(edad.begin(),pupacion.begin())),
                 thrust::make_zip_iterator(thrust::make_tuple(edad.begin() +  N,pupacion.begin() + N)),
-                poblacion_acuaticos()
+                poblacion_2()
             );
 
 	return ac;
@@ -415,88 +409,74 @@ std::cout << "inicializacion lista" << std::endl;
 	int adultos(int dia){
 
 	int N=N_mobil[0];
-	
+	//el predicado poblacion_1()corresponde a adultos
 	    int ad=thrust::count_if(
                 thrust::make_zip_iterator(thrust::make_tuple(edad.begin(),pupacion.begin())),
                 thrust::make_zip_iterator(thrust::make_tuple(edad.begin() +  N,pupacion.begin() + N)),
-                poblacion_adultos()
+                poblacion_1()
             );
 
 		return ad;
-	};
+	};	
 
 	//envejecer población
-
    	void envejecer(int dia){
 	int N=N_mobil[0];
         envejecer_kernel<<<(N + 256-1)/256,256>>>(raw_estado,raw_edad,raw_pupacion,raw_N_mobil,dia);
+        cudaDeviceSynchronize();
 	}; 
-
-	// Envejezco a toda la población un día (aumento en 1): recorre los bichos y les aumenta la edad en una unidad
-	void envejecerthrust(){
-	int N=N_mobil[0];
-	using namespace thrust::placeholders;
-	//envejecen las que estan vivas por eso hasta N 	
-	thrust::transform(edad.begin(),edad.begin()+N, edad.begin(),_1+1);
-	
-	};
 };
+
 
 
 int main(){
 
- 	std::ofstream outfile, outfile1, outfile2;
-   	outfile.open("Poblacion_total_GPU.dat");
+	std::ofstream outfile, outfile1, outfile2;
+    	outfile.open("Poblacion_total_GPU.dat");
    	outfile1.open("Poblacion_adultos_GPU.dat");
-    outfile2.open("Poblacion_acuaticos_GPU.dat");
+    	outfile2.open("Poblacion_acuaticos_GPU.dat");
 
-	int descach=round(NUMEROTACHOS*PROP);//cantidad de tachos que vacรญo con la propaganda 
-	
+	int descach=round(NUMEROTACHOS*PROP);//cantidad de tachos que vacío con la propaganda
+
 	gpu_timer Reloj_GPU;
 	Reloj_GPU.tic();
-    
-    bichos mosquitas(NINICIAL);
 
+    	bichos mosquitas(NINICIAL);
+    //cambié el orden de las funciones y las ordené de acuerdo  al código C++ y Fortran, el orden que tenia anteriormente afectaba el resultado
+    
 	for(int dia = 1; dia <= NDIAS; dia++){
-	int tovip=tiempo_entre_oviposiciones(dia);
+	std::cout << "DIA" << dia << std::endl;
+    	int tovip=tiempo_entre_oviposiciones(dia);
 	
-	std::cout << "reproducir" << std::endl;
-	mosquitas.reproducir(dia,tovip);
-	
-	//std::cout << "matar" << std::endl;
+	std::cout << "matar" << std::endl;
 	mosquitas.mortalidades(dia);//fusione muerte x vejez con mortalidades varias en un solo kernel
 
 	std::cout << "descacharrar" << std::endl;
 	mosquitas.descacharrado(dia,descach); 
-	
-	std::cout << "envejezco poblacion" << std::endl;
-	mosquitas.envejecerthrust();
-	
-	
-	std::cout << "recalculo indice de mosquitas vivas" << std::endl;
-	mosquitas.recalcularN(); 
 
-	std::cout << "DIA" << dia << std::endl;
+	std::cout << "reproducir" << std::endl;
+	mosquitas.reproducir(dia,tovip);
+
+	std::cout << "recalcular indice de mosquitas vivas" << std::endl;
+	mosquitas.recalcularN(); 
 
 	int vivas=mosquitas.vivos(dia);
 	int adultos=mosquitas.adultos(dia);
 	int acuaticos=mosquitas.acuaticos(dia);
+
 	outfile << dia << "\t" << vivas << std::endl;
 	outfile1 << dia << "\t" << adultos << std::endl;
 	outfile2 << dia << "\t" << acuaticos << std::endl;
-	 
 
+	std::cout << "envejecer poblacion" << std::endl;
+	mosquitas.envejecer(dia);
+	
 	}
-
     double t=Reloj_GPU.tac()/60000; //de milisegundos -> minutos
     printf("Tiempo en GPU: %lf minutos\n",t);
-
 //cierro archivos
 outfile.close();
 outfile1.close();
 outfile2.close();
-
-return 0;
-
-
+return 0;							
 }// end for main
--- mosquitas_CUDA/parametros.h
+++ mosquitas_CUDA/parametros.h
@@ -1,34 +1,36 @@
 /*
 Macros utiles para CPU y GPU
-Relaciones a tener en cuenta para modificar parámetros
+Relaciones a tener en cuenta para modificar parรกmetros
 - NUMEROTACHOS=Ninicial
 */
-#define NINICIAL 		    1		//número inicial de mosquitos para codigo GPU	
-#define NUMEROTACHOS		1		//máximo número de tachos 
-#define NUMERODEHUEVOS		10 		//número de huevos por oviposicion	
-#define MAXIMONUMEROBICHOS	80000	//número maximo de huevos
 
-#define ESTADOMUERTO    	1
-#define ESTADOVIVO	        0
-#define NDIAS			    700    //cantidad de dias en el año
+#define NINICIAL	    	5
+#define NUMEROTACHOS		5
+#define MAXIMONUMEROBICHOS	80000
 
-#define MORHUE              0.01   	//mortalidad de huevos
-#define MORLAR              0.01    //mortalidad de larvas 
-#define MORPUP              0.01   	//mortalidad de pupas
-#define MORAD               0.01 	//mortalidad diaria adultas
-#define MORACU              0.03	//morhue+MORLAR+MORPUP;
-#define MORPUPAD            0.17	//pupas que no se vuelven adultas
-#define TOVIP1              2   	//tiempo entre dos oviposiciones (T=30)
-#define TOVIP2a             3     	//tiempo entre dos oviposiciones (T=25)
-#define TOVIP2b             4   	//tiempo entre dos oviposiciones (T=25)
-#define TOVIP3              30  	//tiempo ente dos oviposiciones (T=18)
-#define SAT                 800	    //numero maximo de huevos en cada tacho
-#define PROP                0.6	    //efectividad de la propaganda
+#define ESTADOMUERTO		1
+#define ESTADOVIVO		    0
+#define NDIAS			    400
 
+#define MORHUE 			    0.01   	//mortalidad de huevos
+#define MORLAR 			    0.01    //mortalidad de larvas 
+#define MORPUP 			    0.01   	//mortalidad de pupas
+#define MORAD 			    0.01 	//mortalidad diaria adultas
+#define MORACU 			    0.03	//morhue+morlar+morpup;
+#define MORPUPAD 		    0.17	//pupas que no se vuelven adultas
+#define TPUPAD	 		    17  	//pupas se vuelven adultas a los 17 dias en invierno****
+#define TOVIP1 			    2	    //tiempo entre dos oviposiciones (T=30)
+#define TOVIP2a 		    3  	    //tiempo entre dos oviposiciones (T=25)
+#define TOVIP2b			    4   	//tiempo entre dos oviposiciones (T=25)
+#define TOVIP3 			    30	    //tiempo ente dos oviposiciones (T=18)
+#define SAT 			    800     //saturaciรณn de huevos por tacho
+#define PROP 			    0.6	    //efectividad de la propaganda
 
 #define TPUPAD  		    17  	//pupas se vuelven adultas a los 17 dias en invierno****
 
-#define SEMILLAGLOBAL	    22344888
-//#define SEMILLAGLOBAL	    33455999
+#define SEMILLAGLOBAL	   	22399989
+//#define SEMILLAGLOBAL	   	12345789
+//#define SEMILLAGLOBAL	   	975
 
 long semilla = -975;  			//semilla para el generador de numeros aleatorios ran2()
+
