8a2e03460cb5078a99a7a4ca09daa419558a7520
GramajoAna <63738296+GramajoAna@users.noreply.github.com>
2021-06-07 12:38:17 -0300

    version actualizada

--- mosquitas_CUDA/parametros.h
+++ mosquitas_CUDA/parametros.h
@@ -1,40 +1,37 @@
 /*
 Macros utiles para CPU y GPU
 Relaciones a tener en cuenta para modificar parรกmetros
-- NUMEROTACHOS=NRO DE MOSQUITAS de manera que se considere una hembra adulta por tacho  
-- NUMEROTACHOS esta distribuido de forma aleatoria en el NUMEROMANZANAS ingresado
+- NUMEROTACHOS=NINICIAL
 */
+#define NINICIAL 		        20	        //número inicial de mosquitos N=125 para 5 mosquitas x manzana.
+#define NUMEROTACHOS		    20          //máximo número de tachos Ntachos=Nmosquitas
+#define MAXIMONUMEROBICHOS	80000000    //número maximo de huevos
+#define LADO           	    2           //Lado de la grilla L=5
+#define NUMEROMANZANAS		LADO*LADO   //número de manzanas que sea L*L=5*5=25
 
-#define NUMEROTACHOS		25
-#define NUMEROMANZANAS      25
-#define MAXIMONUMEROBICHOS	60000000
+#define ESTADOMUERTO		  1
+#define ESTADOVIVO	    	0
+#define NDIAS		    	    400	        //nro de dias en el año
+#define NREALIZACIONES		1	        //nro de realizaciones para promediar poblaciones con diferente semillas
 
-#define ESTADOMUERTO		1
-#define ESTADOVIVO		    0
-#define NDIAS			    400
-
-#define MORHUE 			    0.01   	//mortalidad de huevos
-#define MORLAR 			    0.01    //mortalidad de larvas 
-#define MORPUP 			    0.01   	//mortalidad de pupas
-#define MORAD 			    0.01 	//mortalidad diaria adultas
-#define MORACU 			    0.03	//morhue+morlar+morpup;
-#define MORPUPAD 		    0.17	//pupas que no se vuelven adultas
-#define TPUPAD	 		    17  	//pupas se vuelven adultas a los 17 dias en invierno****
-#define TOVIP1 			    2	    //tiempo entre dos oviposiciones (T=30)
-#define TOVIP2a 		    3  	    //tiempo entre dos oviposiciones (T=25)
-#define TOVIP2b			    4   	//tiempo entre dos oviposiciones (T=25)
-#define TOVIP3 			    30	    //tiempo ente dos oviposiciones (T=18)
-#define SAT 			    800     //saturaciรณn de huevos por tacho
-#define PROP 			    0.6	    //efectividad de la propaganda
-
-#define TPUPAD  		    17  	//pupas se vuelven adultas a los 17 dias en invierno****
+#define MORHUE 		        0.01   	    //mortalidad de huevos
+#define MORLAR 		        0.01       //mortalidad de larvas 
+#define MORPUP 		        0.01   	    //mortalidad de pupas
+#define MORAD 	    	  	0.01 	    //mortalidad diaria adultas
+#define MORACU 	        	0.03	    //morhue+morlar+morpup;
+#define MORPUPAD 	    	  0.17	    //pupas que no se vuelven adultas
+#define TPUPAD	    	  	17  	    //pupas se vuelven adultas a los 17 dias en invierno****
+#define TOVIP1 	        	2	        //tiempo entre dos oviposiciones (T=30)
+#define TOVIP2a     	  	3  	        //tiempo entre dos oviposiciones (T=25)
+#define TOVIP2b		        4   	    //tiempo entre dos oviposiciones (T=25)
+#define TOVIP3 		        30	        //tiempo ente dos oviposiciones (T=18)
+#define SAT 		        	800         //saturación de huevos por tacho
+#define PROP 		    	    0.6         //efectividad de la propaganda
+    
+#define TPUPAD  	    	  17  	    //pupas se vuelven adultas a los 17 dias en invierno****
 
 //#define SEMILLAGLOBAL	   	22399989
-#define SEMILLAGLOBAL	   	123456789
+#define SEMILLAGLOBAL	   	  123456789
 //#define SEMILLAGLOBAL	   	975
 
-//******************************************** NUEVO ******************************************************************
-long semilla = -739;  			//semilla para el generador de numeros aleatorios ran2()
-int L=sqrt(NUMEROMANZANAS);     //nro para la dimensión de la matriz de manzanas
-int filas = L;
-int columnas = L;
+//long semilla = -739;  			//semilla para el generador de numeros aleatorios ran2()
